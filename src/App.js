0e445bccba371627b6ab9c961b4af90f915f9544
mrinnnmoy <mrinmoy202000@gmail.com>
2023-01-03 06:49:11 +0530

    build hero-component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Hero from './components/Hero';
 import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <Hero />
     </div>
   );
 }
