8d6e4f54cdad1ea9a9f60c4d8aa8d5cf14ec2804
mrinnnmoy <mrinmoy202000@gmail.com>
2023-01-03 06:38:20 +0530

    build nav-component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
 import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      <Nav/>
     </div>
-  )
-};
+  );
+}
 
 export default App;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import {AiFillHeart , AiOutlineSearch, AiOutlineShoppingCart} from 'react-icons/ai';
+import React from 'react'
+import { AiFillHeart, AiOutlineSearch, AiOutlineShoppingCart } from 'react-icons/ai'
 
 const Nav = () => {
 
   return (
     <nav className="w-11/12 xl:w-4/5 m-auto flex justify-between py-5">
-        <h1 className="text-2xl font-semibold text-rose-400">Ecom</h1>
-        <div>
-            <ul className="flex space-x-8 text-base hidden md:flex">
-                <li>Home</li>
-                <li>Products</li>
-                <li>Services</li>
-                <li>About</li>
-            </ul>
-        </div>
-        <div className="flex space-x-8">
-            <AiFillHeart size={"1.5rem"} />
-            <AiOutlineSearch size={"1.5rem"} />
-            <AiOutlineShoppingCart size={"1.5rem"} />
-        </div>
+      <h1 className="text-2xl font-semibold text-rose-400 cursor-pointer">Ecom</h1>
+      <div>
+        <ul className="flex space-x-8 text-base hidden md:flex">
+          <li className="cursor-pointer hover:text-red-400">Home</li>
+          <li className="cursor-pointer hover:text-red-400">Products</li>
+          <li className="cursor-pointer hover:text-red-400">Services</li>
+          <li className="cursor-pointer hover:text-red-400">About</li>
+        </ul>
+      </div>
+      <div className="flex space-x-8">
+        <AiOutlineSearch size={"1.5rem"} className="cursor-pointer hover:fill-green-500" />
+        <AiFillHeart size={"1.5rem"} className="cursor-pointer hover:fill-red-500" />
+        <AiOutlineShoppingCart size={"1.5rem"} className="cursor-pointer hover:fill-blue-500" />
+      </div>
     </nav>
   )
-};
+}
 
-export default Nav;
+export default Nav
